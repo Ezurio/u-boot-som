1c97bce48cb68365694a469082e1c54ae418a626
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-12-21 13:13:59 -0500

    nitrogen: imx95: imx93: Fixes for radio init and others
    PROD-44914

--- board/summit/nitrogen_imx95/nitrogen_imx95.c
+++ board/summit/nitrogen_imx95/nitrogen_imx95.c
@@ -5,6 +5,7 @@
 
 #include <init.h>
 #include <asm/arch/clock.h>
+#include <env.h>
 
 #include "../common/common.h" 
 
@@ -27,6 +28,7 @@ int board_late_init(void)
 
 	return 0;
 }
+
 int board_phys_sdram_size(phys_size_t *size)
 {
 	*size = PHYS_SDRAM_SIZE + PHYS_SDRAM_2_SIZE;
