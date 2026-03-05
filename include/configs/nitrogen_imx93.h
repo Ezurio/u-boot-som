bf4b7ee4b96995a9d5bdd2cd19441047358ea991
Chris Trowbridge <chris.trowbridge@ezurio.com>
2026-03-05 14:40:43 -0500

    nitrogen_imx93: Enable AHAB support
    Refs: PROD-55003

--- include/configs/nitrogen_imx93.h
+++ include/configs/nitrogen_imx93.h
@@ -6,6 +6,8 @@
 #ifndef __NITROGEN_IMX93_H
 #define __NITROGEN_IMX93_H
 
+#include <asm/arch/imx-regs.h>
+
 #define CFG_SYS_UBOOT_BASE	\
 	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
 
