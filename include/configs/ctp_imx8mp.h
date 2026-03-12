8158c71aba09889e4139d065ff54c2706e7ceac8
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2026-03-12 13:46:27 -0400

    ctp: Fixed ifx regdomain update
    PROD-27490

--- include/configs/ctp_imx8mp.h
+++ include/configs/ctp_imx8mp.h
@@ -43,7 +43,7 @@
 
 #define CFG_ENV_FLAGS_LIST_STATIC \
 	CFG_ENV_FLAGS_LIST_BOOTCOUNT CFG_ENV_FLAGS_LIST_NET \
-	"bootside:sw,rescueside:sw,fips:dw,fips_wifi:dw,version:sw,conf:sw"
+	"bootside:sw,rescueside:sw,fips:dw,fips_wifi:dw,version:sw,conf:sw,RegDomain:sw"
 #endif
 
 #endif
